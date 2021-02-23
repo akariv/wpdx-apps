1702eaa04a29292a8796f567f60f7bc38da193b0
Adam Kariv <adam.kariv@gmail.com>
2021-02-23 15:16:38 +0200

    toggle

--- projects/data-robot/src/app/main-page/main-page.component.ts
+++ projects/data-robot/src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  layer = '';
-  
+  LAYERS = [
+    'datarobot-slim-year-0',
+    'datarobot-slim-year-1',
+    'datarobot-slim-year-3',
+  ]
+  _layer = this.LAYERS[0];
+  map: mapboxgl.Map;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  set layer(value) {
+    this._layer = value;
+    for (const layer of this.LAYERS) {
+      if (value === layer || value === 'combined') {
+        this.map.setLayoutProperty(layer, 'visibility', 'visible');
+      }
+    }
+    for (const layer of this.LAYERS) {
+      if (value !== layer && value !== 'combined') {
+        this.map.setLayoutProperty(layer, 'visibility', 'none');
+      }
+    }
+    console.log(this.map);
+  }
+
+  get layer() {
+    return this._layer;
   }
 
 }
