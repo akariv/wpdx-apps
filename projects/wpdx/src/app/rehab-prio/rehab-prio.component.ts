b4b540ba27155b06db16e0cfd13bd11a6435a9c0
Adam Kariv <adam.kariv@gmail.com>
2022-01-25 16:36:52 +0200

    Add labels and pie charts independently

--- projects/wpdx/src/app/rehab-prio/rehab-prio.component.ts
+++ projects/wpdx/src/app/rehab-prio/rehab-prio.component.ts
@@ -559,8 +559,8 @@ export class RehabPrioComponent implements OnInit {
     }
     console.log('ADMAN FILT', admanFilt);
     for (const layer of ['adm-analysis', 'adm-analysis-borders', 'adm-analysis-labels']) {
-      this.rpState.map.setLayoutProperty(layer, 'visibility',
-          layer === 'adm-analysis-labels' ? (this.rpState.show_adman_labels ? visibility : 'none') : visibility);
+      this.rpState.map.setLayoutProperty(layer, 'visibility', visibility);
+      this.rpState.map.setPaintProperty('adm-analysis-labels', 'opacity', this.rpState.show_adman_labels ? 1 : 0);
       this.rpState.map.setFilter(layer, ['all',
         ['==', ['get', 'adm_level'], ['literal', admanLevel]],
         ...admanFilt
