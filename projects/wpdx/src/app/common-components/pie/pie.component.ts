bfb4fba5e49ce364f120b483271dadf5eac25a64
Camila <camila.labarca@ug.uchile.cl>
2022-02-23 09:25:12 +0200

    adm popup waterpoint functionality pie chart

--- projects/wpdx/src/app/common-components/pie/pie.component.ts
+++ projects/wpdx/src/app/common-components/pie/pie.component.ts
@@ -8,23 +8,94 @@ import * as d3 from 'd3';
 })
 export class PieComponent implements OnChanges, AfterViewInit {
 
-  @ViewChild('bar') svgElement: ElementRef;
+  @ViewChild('pie') svgElement: ElementRef;
+  @Input() data: any;
 
+
+  // data = [
+  //   {
+  //     name: "text1",
+  //     value: "95"
+  //   },
+  //   {
+  //     name: "text1",
+  //     value: "4"
+  //   },
+  //   {
+  //     name: "text3",
+  //     value: "1"
+  //   }
+  // ];
+
+  chartId;
   svg;
-  
-  margin = {top: 20, right: 10, bottom: 70, left: 40};
-  width = 270 - this.margin.left - this.margin.right;
-  height = 360 - this.margin.top - this.margin.bottom;
-  radius = Math.min(this.width, this.height) / 2;
+  margin = 25;
+  width = 650;
+  height = 350;
+
+  radius = Math.min(this.width, this.height) / 2 - this.margin;
+  colors;
 
   createSvg(): void {
     this.svgElement.nativeElement.innerHTML = '';
-    this.svg = d3.select('figure.bar')
-    .append('svg')
-    .append('g')
-    .attr('class', 'slices')
-    .attr('class', 'labels')
-    .attr('class', 'lines');
+    this.svg = d3
+      .select("figure.pie")
+      .append("svg")
+      .attr("viewBox", `0 0 ${this.width} ${this.height}`)
+      // .attr('width', this.width)
+      //.attr('height', this.height)
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + this.width / 4 + "," + this.height / 2 + ")"
+      );
+  }
+
+  createColors(data = this.data): void {
+    this.colors = d3
+      .scaleOrdinal()
+      .domain(data.map(d => d.value.toString()))
+      .range([
+        "#6773f1",
+        "#32325d",
+        "#6162b5",
+        "#6586f6",
+        "#8b6ced",
+        "#1b1b1b",
+        "#212121"
+      ]);
+  }
+
+  drawChart(data=this.data){
+    // Compute the position of each group on the pie
+    const pie = d3.pie<any>().value((d: any) => Number(d.value));
+    const data_ready = pie(data);
+
+    let outerArc = d3
+    .arc()
+    .innerRadius(this.radius * 0.9)
+    .outerRadius(this.radius * 0.9);
+
+    let arc = d3
+      .arc()
+      .innerRadius(this.radius * 0.5) // This is the size of the donut hole
+      .outerRadius(this.radius * 0.8);
+
+      this.svg
+      .selectAll("pieces")
+      .data(data_ready)
+      .enter()
+      .append("path")
+      .attr(
+        "d",
+        d3
+          .arc()
+          .innerRadius(0)
+          .outerRadius(this.radius)
+      )
+      .attr("fill", (d, i) => (d.data.color ? d.data.color : this.colors(i)))
+      .attr("stroke", "#121926")
+      .style("stroke-width", "1px");
   }
 
   constructor() { }
@@ -34,7 +105,8 @@ export class PieComponent implements OnChanges, AfterViewInit {
       return;
     }
     this.createSvg();
-    // this.drawHistogram();
+    this.createColors();
+    this.drawChart();
   }
 
   ngAfterViewInit() {
