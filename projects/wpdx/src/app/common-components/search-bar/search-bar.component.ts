23eb91a60590eb881e5dd3b798649c63faf3c3e7
Camila <camila.labarca@ug.uchile.cl>
2022-07-06 11:43:55 -0400

    changed order of search options and fixed autocomplete

--- projects/wpdx/src/app/common-components/search-bar/search-bar.component.ts
+++ projects/wpdx/src/app/common-components/search-bar/search-bar.component.ts
@@ -35,23 +35,44 @@ export class SearchBarComponent implements OnInit{
   }
 
   private _filter(value: string): string[] {
+    
     const filterValue = value.toLowerCase();
-    const filteredArray = this.options.filter(option => option.display.toLowerCase().includes(filterValue));
+    const splittedValue = filterValue.split(" "); 
+    let filteredArray = this.options;
+    for (const val of splittedValue){
+      filteredArray = filteredArray.filter(option => option.display.toLowerCase().includes(val));
+    }
     const slicedArray = filteredArray.slice(0, 50);
     return slicedArray;
   }
   
   processDBResults(items) {
-    items = this.groupBy(items, ['clean_country_name', 'clean_adm1', 'clean_adm2', 'clean_adm3'], 'clean_adm4');
-    items = this.groupBy(items, ['clean_country_name', 'clean_adm1', 'clean_adm2'], 'clean_adm3');
-    items = this.groupBy(items, ['clean_country_name', 'clean_adm1'], 'clean_adm2');
-    items = this.groupBy(items, ['clean_country_name'], 'clean_adm1');
-    for (const item of items){
+
+    const countries = this.groupBy(items, ['clean_country_name'], 'clean_adm1');
+    const adm1 = this.groupBy(items, ['clean_country_name', 'clean_adm1'], 'clean_adm2');
+    const adm2 = this.groupBy(items, ['clean_country_name', 'clean_adm1', 'clean_adm2'], 'clean_adm3');
+    const adm3 = this.groupBy(items, ['clean_country_name', 'clean_adm1', 'clean_adm2', 'clean_adm3'], 'clean_adm4');
+    const adm4 = items;
+    for (const item of countries){
       const option = {
-        'display': item.clean_country_name,
+            'display': item.clean_country_name,
+            'state': {
+              country_name: item.clean_country_name,
+              adm1: undefined,
+              adm2: undefined,
+              adm3: undefined,
+              adm4: undefined,
+              bounds: item.bounds
+            }
+      };
+      this.options.push(option);
+    }
+    for (const item of adm1) {
+      const option = {
+        'display': item.clean_adm1 + ', ' + item.clean_country_name,
         'state': {
           country_name: item.clean_country_name,
-          adm1: undefined,
+          adm1: item.clean_adm1,
           adm2: undefined,
           adm3: undefined,
           adm4: undefined,
@@ -59,62 +80,48 @@ export class SearchBarComponent implements OnInit{
         }
       };
       this.options.push(option);
-      for (const item1 of item.items){
-        const option = {
-          'display': item1.value + ', ' + item1.clean_country_name,
-          'state': {
-            country_name: item1.clean_country_name,
-            adm1: item1.value,
-            adm2: undefined,
-            adm3: undefined,
-            adm4: undefined,
-            bounds: item1.bounds
-          }
-        };
-        this.options.push(option);
-        for (const item2 of item1.items){
-          const option = {
-            'display': item2.value +', '+ item2.clean_adm1 + ', ' + item2.clean_country_name,
-            'state': {
-              country_name: item2.clean_country_name,
-              adm1: item2.clean_adm1,
-              adm2: item2.value,
-              adm3: undefined,
-              adm4: undefined,
-              bounds: item2.bounds
-            }
-          };
-          this.options.push(option);
-          for (const item3 of item2.items){
-            const option = {
-              'display': item3.value + ', ' + item3.clean_adm2 +', '+ item3.clean_adm1 + ', ' + item3.clean_country_name,
-              'state': {
-                country_name: item3.clean_country_name,
-                adm1: item3.clean_adm1,
-                adm2: item3.clean_adm2,
-                adm3: item3.value,
-                adm4: undefined,
-                bounds: item3.bounds
-              }
-            };
-            this.options.push(option);
-            for (const item4 of item3.items){
-              const option = {
-                'display': item4.value +', '+ item4.clean_adm3 + ', ' + item4.clean_adm2 +', '+ item4.clean_adm1 + ', ' + item4.clean_country_name,
-                'state': {
-                  country_name: item4.clean_country_name,
-                  adm1: item4.clean_adm1,
-                  adm2: item4.clean_adm2,
-                  adm3: item4.clean_adm3,
-                  adm4: item4.value,
-                  bounds: item4.bounds
-                }
-              };
-              this.options.push(option);
-            }
-          }
+    }
+    for (const item of adm2) {
+      const option = {
+        'display': item.clean_adm2 +', '+ item.clean_adm1 + ', ' + item.clean_country_name,
+        'state': {
+          country_name: item.clean_country_name,
+          adm1: item.clean_adm1,
+          adm2: item.clean_adm2,
+          adm3: undefined,
+          adm4: undefined,
+          bounds: item.bounds
         }
-      }
+      };
+      this.options.push(option);
+    }
+    for (const item of adm3){
+      const option = {
+        'display': item.clean_adm3 + ', ' + item.clean_adm2 +', '+ item.clean_adm1 + ', ' + item.clean_country_name,
+        'state': {
+          country_name: item.clean_country_name,
+          adm1: item.clean_adm1,
+          adm2: item.clean_adm2,
+          adm3: item.clean_adm3,
+          adm4: undefined,
+          bounds: item.bounds
+        }
+      };
+      this.options.push(option);
+    }
+    for (const item of adm4) {
+      const option = {
+        'display': item.clean_adm4 + ', ' + item.clean_adm3 + ', ' + item.clean_adm2 +', '+ item.clean_adm1 + ', ' + item.clean_country_name,
+        'state': {
+          country_name: item.clean_country_name,
+          adm1: item.clean_adm1,
+          adm2: item.clean_adm2,
+          adm3: item.clean_adm3,
+          adm4: item.clean_adm4,
+          bounds: item.bounds
+        }
+      };
+      this.options.push(option);
     }
   }
 
