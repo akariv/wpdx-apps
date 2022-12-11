c4bf870551e59dd38204fcc08f077e2b61d91d76
Adam Kariv <adam.kariv@gmail.com>
2022-12-11 15:18:24 +0200

    Read server parameters

--- projects/wpdx/src/app/rehab-prio/rehab-prio-preview/rehab-prio-preview.component.ts
+++ projects/wpdx/src/app/rehab-prio/rehab-prio-preview/rehab-prio-preview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StateService } from '../../common-components/state.service';
 import { DbService } from '../../db.service';
+import { ServerParamsService } from '../../server-params.service';
 import { RehabPrioComponent } from '../rehab-prio.component';
 import { RpStateService } from '../rp-state.service';
 
@@ -12,8 +13,8 @@ import { RpStateService } from '../rp-state.service';
 })
 export class RehabPrioPreviewComponent extends RehabPrioComponent {
 
-  constructor(private db_: DbService, private state_: StateService, private rpState_: RpStateService, public dialog: MatDialog) {
-    super(db_, state_, rpState_, dialog);
+  constructor(private db_: DbService, private state_: StateService, private rpState_: RpStateService, public dialog: MatDialog, private params_: ServerParamsService) {
+    super(db_, state_, rpState_, dialog, params_);
     this.preview = true;
   }
 }
