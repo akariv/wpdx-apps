10d335e64309c7635a0053cabd07cb772f0024b3
Adam Kariv <adam.kariv@gmail.com>
2023-05-01 16:43:59 +0300

    Sort by would gain access by default

--- projects/wpdx/src/app/rehab-prio/rp-state.service.ts
+++ projects/wpdx/src/app/rehab-prio/rp-state.service.ts
@@ -124,7 +124,7 @@ export class RpStateService {
   }
 
   get sort_by() {
-    return this.state.getProp('sort_by') || this.sort_options[0].value;
+    return this.state.getProp('sort_by') || this.sort_options[3].value; // Sort by would gain access by default
   }
 
   set show_point_counts(value) {
